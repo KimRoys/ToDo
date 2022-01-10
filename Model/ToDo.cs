b40005121474144c3af504fe944d127ce209eec5
Kim Roysdotter <kim.roysdotter@gmail.com>
2022-01-10 20:19:20 +0100

    ToDo class + tests added

---
+++ Model/ToDo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo_App.Model
+{
+    public class ToDo
+    {
+        private readonly int todoId;
+        private string description;
+        private bool done;
+        private Person assignee;
+
+        public ToDo(int todoId)
+        {
+            this.todoId = todoId;
+        }
+
+        public int ToDoId
+        {
+            get => todoId;
+        }
+
+        public ToDo(int todoId, string description)
+        {
+            this.todoId = todoId;
+            this.description = description;
+        }
+
+        public string Description
+        {
+            get => description;
+            set => description = value;
+        }
+
+        public bool Done
+        {
+            get => done;
+            set => done = value;
+        }
+
+        public Person Person
+        {
+            get => assignee;
+            set => assignee = value;
+        }
+    }
+}
