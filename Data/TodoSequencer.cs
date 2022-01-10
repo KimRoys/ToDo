966bddf7a4a9b12884f0c95a93118e1c7cb44918
Kim Roysdotter <kim.roysdotter@gmail.com>
2022-01-10 21:05:05 +0100

    TodoSequencer + tests added

---
+++ Data/TodoSequencer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo_App.Data
+{
+    public class TodoSequencer
+    {
+        private static int toDoId = 0;
+
+        public static int nextToDoId()
+        {
+            return ++toDoId;
+        }
+
+        public static int reset()
+        {
+            toDoId = 0;
+            return toDoId;
+        }
+    }
+}
