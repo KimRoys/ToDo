54380d9d59961371fb2a1dbe487561174af508aa
KimRoys <85109206+KimRoys@users.noreply.github.com>
2022-01-12 12:43:18 +0100

    Add files via upload
    Ny uppladdning av ToDo-App

---
+++ ToDo App/Model/ToDo.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo_App.Model
+{
+    public class ToDo
+    {
+        private readonly int todoId;
+        private string description;
+        private bool done;
+        private Person assignee;
+
+        public int ToDoId => todoId;
+        public ToDo(int todoId)
+        {
+            this.todoId = todoId;
+        }
+
+
+        public ToDo(int todoId, string description) : this()
+        {
+            this.todoId = todoId;
+            this.description = description;
+        }
+
+        public string Description
+        {
+            get => description;
+            set
+            {
+                if (value == null)              // Check for null assignment
+                {
+                    throw new ArgumentNullException("Description", "property Description cannot be assigned a null value!");
+                }
+
+                description = value;
+            }
+        }
+
+        public bool Done
+        {
+            get => done;
+            set => done = value;
+        }
+
+        public Person Assignee 
+        { 
+            get => assignee; 
+            set => assignee = value; 
+        
+        }
+
+        public ToDo()
+        {
+            assignee = null;
+            done = false;
+            todoId = 0;
+            description = "";
+        }
+    }
+}
---
+++ ToDo App/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ToDo_App.Model;
+
+namespace ToDo_App
+{
+    class Program
+    {
+        public static Person person { get; private set; }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            Console.WriteLine();
+            Console.ReadLine();
+                  
+        }
+    }
+}
